8545181bfcd05e2aac30e1c109ea82ded36839f9
Jose Danie <Jodacam2@gmail.com>
2018-10-09 15:05:41 +0200

    Corregido html

--- js/custom.js
+++ js/custom.js
@@ -33,48 +33,48 @@
 
   $(function() {
 
-    $.mbBgndGallery.buildGallery({
-      containment: "#intro",
-      timer: 3000,
-      effTimer: 1000,
-      controls: "#controls",
-      grayScale: false,
-      shuffle: false,
-      preserveWidth: false,
-      effect: "fade",
-      effect: {
-        enter: {
-          left: 0,
-          opacity: 0
-        },
-        exit: {
-          left: 0,
-          opacity: 0
-        },
-        enterTiming: "ease-in",
-        exitTiming: "ease-in"
-      },
+    // $.mbBgndGallery.buildGallery({
+    //   containment: "#intro",
+    //   timer: 3000,
+    //   effTimer: 1000,
+    //   controls: "#controls",
+    //   grayScale: false,
+    //   shuffle: false,
+    //   preserveWidth: false,
+    //   effect: "fade",
+    //   effect: {
+    //     enter: {
+    //       left: 0,
+    //       opacity: 0
+    //     },
+    //     exit: {
+    //       left: 0,
+    //       opacity: 0
+    //     },
+    //     enterTiming: "ease-in",
+    //     exitTiming: "ease-in"
+    //   },
 
-      // If your server allow directory listing you can use:
-      // (however this doesn't work locally on your computer)
+    //   // If your server allow directory listing you can use:
+    //   // (however this doesn't work locally on your computer)
 
-      //folderPath:"testImage/",
+    //   //folderPath:"testImage/",
 
-      // else:
+    //   // else:
 
-      images: [
-        "img/bgslides/4.jpg",
-        "img/bgslides/5.jpg",
-        "img/bgslides/6.jpg"
-      ],
+    //   images: [
+    //     "img/bgslides/4.jpg",
+    //     "img/bgslides/5.jpg",
+    //     "img/bgslides/6.jpg"
+    //   ],
 
-      onStart: function() {},
-      onPause: function() {},
-      onPlay: function(opt) {},
-      onChange: function(opt, idx) {},
-      onNext: function(opt) {},
-      onPrev: function(opt) {}
-    });
+    //   onStart: function() {},
+    //   onPause: function() {},
+    //   onPlay: function(opt) {},
+    //   onChange: function(opt, idx) {},
+    //   onNext: function(opt) {},
+    //   onPrev: function(opt) {}
+    // });
 
 
   });
