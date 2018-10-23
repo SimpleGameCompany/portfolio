019461f3e1807087ea99f8b4c1ddbf904ebc43ff
rgastonv <rgastonv@hotmail.es>
2018-10-23 16:29:44 +0200

    Cambios en la carga de imágenes

--- Games/ProEvolutionQuidditch/src/states/boot.js
+++ Games/ProEvolutionQuidditch/src/states/boot.js
@@ -7,7 +7,9 @@ ProEvolutionQuidditch.bootState = function(game) {
 ProEvolutionQuidditch.bootState.prototype = {
 
     preload: function() {
-        
+        //Prepare images for preload screen:
+        game.load.image('background', '/../../assets/images/background/background.png');        
+        game.load.image('loading1', '/../../assets/images/text/loading1.png');
     },
 
     create: function() {
